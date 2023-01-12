a41f666adb94ca4985501ccb092d685a8b95efba
PylypMax <99202559+PylypMax@users.noreply.github.com>
2023-01-12 15:35:56 +0200

    Add files via upload

---
+++ SpeakGoodBye.js
@@ -0,0 +1,13 @@
+(function speak(name) {
+  var speakWord = "Good Bye";
+  console.log(speakWord + " " + name);
+})();
+
+var speakWord = "Good Bye";
+
+var byeSpeaker = {
+  speak : function speak(name) {
+    var speakWord = "Good Bye";
+    console.log(speakWord + " " + name);
+  }
+}
---
+++ SpeakHello.js
@@ -0,0 +1,15 @@
+
+(function speak(name) {
+  var speakWord = "Hello";
+  console.log(speakWord + " " + name);
+})();
+
+var speakWord = "Hello";
+
+var helloSpeaker = {
+  speak : function speak(name) {
+    var speakWord = "Hello";
+    console.log(speakWord + " " + name);
+  }
+}
+
